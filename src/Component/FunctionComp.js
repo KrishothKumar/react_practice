f59009d70b0acaa257a8f58b4f67ca005ba9e0ed
KrishothKumar <Krishothkumar@hotmail.com>
2020-03-22 18:25:25 +0530

    CREATED: Function Component

---
+++ src/Component/FunctionComp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// export function FunctionComp() { if export is removed use this.
+// function FunctionComp() {
+//     return <h1>Hello Krishoth Kumar</h1>
+// }
+
+const FunctionComp = () => <h1>Hello Krishoth Kumar</h1>
+
+export default FunctionComp  //if is removed add export in function
