092814aaff684cbb60f77dbc6ff6a621b8c9b3b5
Krishoth Kumar <KrishothKumar@hotmail.com>
2023-07-07 21:50:43 +0530

    Importing Component in App.js file.

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,11 +1,14 @@
+import Header from "./Header";
+
 function App() {
-  function handleChangeQuotes() {
-    const quote = ["That's one small step for a man, a giant leap for mankind.", 'The love of money is the root of all evil.', 'The only thing we have to fear is fear itself.', 'The truth will set you free.'];
-    return quote[Math.floor(Math.random()*4)];
-  }
+  // function handleChangeQuotes() {
+  //   const quote = ["That's one small step for a man, a giant leap for mankind.", 'The love of money is the root of all evil.', 'The only thing we have to fear is fear itself.', 'The truth will set you free.'];
+  //   return quote[Math.floor(Math.random()*4)];
+  // }
   return (
     <div>
-        <p>{handleChangeQuotes()}</p>
+        {/* <p>{handleChangeQuotes()}</p> */}
+        <Header/>
     </div>
   );
 }
