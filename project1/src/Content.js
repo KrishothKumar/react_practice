ef7d6ccc27fe24d7397f9a6966e14a8d198ca74f
Krishoth Kumar <KrishothKumar@hotmail.com>
2023-08-31 21:20:58 +0530

    useState to Change quote dynamically.

--- project1/src/Content.js
+++ project1/src/Content.js
@@ -3,16 +3,20 @@ import { useState } from "react";
 
 const Content = () => {
     function handleChangeQuotes() {
-        const quote = [
+        const quotes = [
             "That's one small step for a man, a giant leap for mankind.", 
             'The love of money is the root of all evil.', 
             'The only thing we have to fear is fear itself.', 
             'The truth will set you free.'
         ];
-        return quote[Math.floor(Math.random()*4)];
+        return quotes[Math.floor(Math.random()*4)];
     }
 
-    const [count, setCount] = useState(9);
+    const [quote, setQuote] = useState(() => handleChangeQuotes());
+
+    function handleStateQuotes () {
+        setQuote(handleChangeQuotes())
+    }
     // const handleClick = (e) => {
     //     console.log(e.target.innerText);
     // }
@@ -20,24 +24,21 @@ const Content = () => {
     // const handleClick2 = (value) => {
     //     alert(`Hello ${value}`);
     // }
-    const decrementCount = () => {
-        setCount(count - 1);
-    }
+    // const decrementCount = () => {
+    //     setCount(count - 1);
+    // }
 
-    const incrementCount = () => {
-        setCount((precount) => {return precount + 1});
-    }
+    // const incrementCount = () => {
+    //     setCount((precount) => {return precount + 1});
+    // }
 
     return (
         <main>
             {/* <p onDoubleClick={() => handleClick2('Krishoth Kumar')}>{handleChangeQuotes()}</p>
             <button onClick={(e) => {handleClick(e)}}>Click</button> */}
 
-            <p>The truth will set you free.</p>
-            <button>Click</button>
-            <button onClick={decrementCount}>-</button>
-            <span>{count}</span>
-            <button onClick={incrementCount}>+</button>
+            <p>{quote}</p>
+            <button onClick={handleStateQuotes}>Click</button>
         </main>
     )
 }
