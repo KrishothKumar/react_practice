f09ed709f487e82e0213779ab914db43432c1ca6
Krishoth Kumar <KrishothKumar@hotmail.com>
2023-08-31 22:23:19 +0530

    Adding Map and filter usage.

--- project1/src/Content.js
+++ project1/src/Content.js
@@ -2,21 +2,33 @@ import React from "react";
 import { useState } from "react";
 
 const Content = () => {
-    function handleChangeQuotes() {
-        const quotes = [
-            "That's one small step for a man, a giant leap for mankind.", 
-            'The love of money is the root of all evil.', 
-            'The only thing we have to fear is fear itself.', 
-            'The truth will set you free.'
-        ];
-        return quotes[Math.floor(Math.random()*4)];
-    }
+    // function handleChangeQuotes() {
+    //     const quotes = [
+    //         "That's one small step for a man, a giant leap for mankind.", 
+    //         'The love of money is the root of all evil.', 
+    //         'The only thing we have to fear is fear itself.', 
+    //         'The truth will set you free.'
+    //     ];
+    //     return quotes[Math.floor(Math.random()*4)];
+    // }
 
-    const [quote, setQuote] = useState(() => handleChangeQuotes());
+    // const [quote, setQuote] = useState(() => handleChangeQuotes());
 
-    function handleStateQuotes () {
-        setQuote(handleChangeQuotes())
-    }
+    // const number = [-2,-1,0,1,2]
+    // const numberItems = number.map(n => ({number:n}))
+    // const numberItems = number.filter(n => n >= 0)
+    // const numberItems = number.filter(n => n >= 0).map(n => ({number:n}))
+    // console.log(numberItems)
+
+    const [items, setItems] = useState([
+        {id: 1, checked: true, item:"React Learning"}, 
+        {id: 2, checked: true, item:"Youtube surfing"}, 
+        {id: 3, checked: false, item:"PocketFm"}
+    ]);
+
+    // function handleStateQuotes () {
+    //     setQuote(handleChangeQuotes())
+    // }
     // const handleClick = (e) => {
     //     console.log(e.target.innerText);
     // }
@@ -35,10 +47,20 @@ const Content = () => {
     return (
         <main>
             {/* <p onDoubleClick={() => handleClick2('Krishoth Kumar')}>{handleChangeQuotes()}</p>
-            <button onClick={(e) => {handleClick(e)}}>Click</button> */}
+            <button onClick={(e) => {handleClick(e)}}>Click</button> 
 
             <p>{quote}</p>
-            <button onClick={handleStateQuotes}>Click</button>
+            <button onClick={handleStateQuotes}>Click</button>*/}
+            <ul>
+                {items.map((items) => (
+                        <li>
+                            <input type="checkbox" checked={items.checked}/>
+                            <label>{items.item}</label>
+                            <button>Delete</button>
+                        </li>
+                    )
+                )}
+            </ul>
         </main>
     )
 }
--- project1/src/Header.js
+++ project1/src/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
  return (
     <header>
-        <h1>Quotes</h1>
+        <h1>To do List</h1>
     </header>
  )
 }
