90d77ab3992d9cd9adb927b97200eadb172c9b0d
Krishoth Kumar <KrishothKumar@hotmail.com>
2023-09-07 00:11:15 +0530

    Remove multiple Css and add common css.

--- project2/src/Square.js
+++ project2/src/Square.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Square = ({colour, colourHex, isToggle}) => {
   return (
-      <section className="square" style={{textAlign:'center', backgroundColor: colour}}>
-        <p style={{color: isToggle ? '#000' : '#FFF'}}>{ colour }</p>
-        <p style={{color: isToggle ? '#000' : '#FFF'}}>{colourHex ? colourHex : ``}</p>
+      <section className="square" style={{textAlign:'center', backgroundColor: colour, color: isToggle ? '#000' : '#FFF'}}>
+        <p>{ colour }</p>
+        <p>{colourHex ? colourHex : ``}</p>
       </section>
   );
 }
