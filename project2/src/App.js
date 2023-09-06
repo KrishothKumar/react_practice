1aa7ee127e87d9afc9087d467aab0d967b94d368
Krishoth Kumar <KrishothKumar@hotmail.com>
2023-09-06 22:54:31 +0530

    Added color change box with toggle text.

--- project2/src/App.js
+++ project2/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [colour, setColour] = useState();
+  const [toggle, setToggle] = useState('black');
+  const handleInput = (e) => {
+    setColour(e.target.value)
+  }
+
+  const handleToggle = (e) => {
+    if(toggle == 'black'){
+      setToggle('white')
+    } else {
+      setToggle('black')
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='square' style={{textAlign:'center', backgroundColor: colour}}>
+      <p style={{color:toggle}}>{colour ? colour : `Empty`}</p>
+      </div>
+      <input onChange={(e) => handleInput(e)} type='text'/>
+      <button onClick={(e) => handleToggle(e)}> Toggle Test colour</button>
     </div>
   );
 }
