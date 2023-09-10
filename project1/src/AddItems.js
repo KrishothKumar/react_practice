a4c7f8ec0572ab77bd863f488f93b8a9de8eb509
Krishoth Kumar <KrishothKumar@hotmail.com>
2023-09-10 12:32:34 +0530

    Added useRef pratice.

--- project1/src/AddItems.js
+++ project1/src/AddItems.js
@@ -1,14 +1,16 @@
 import React from "react";
-import {FaPlus} from "react-icons/fa"
+import { FaPlus } from "react-icons/fa"
+import { useRef } from "react";
 
 const AddItems = ({newItem, setNewItem, handleSubmit}) => {
+    const inputRef = useRef()
     return (
         <form className="addForm" onSubmit={handleSubmit}>
             <label htmlFor="addItem">Add Item</label>
-            <input id="addItem" autoFocus placeholder="Enter Item" required type="text"
+            <input id="addItem" autoFocus ref={inputRef} placeholder="Enter Item" required type="text"
             value={newItem} onChange={(e) => setNewItem(e.target.value)}
             />
-            <button type="submit" aria-label="Add Item"> <FaPlus /></button>
+            <button type="submit" aria-label="Add Item" onClick={() => {inputRef.current.focus()}}> <FaPlus /></button>
         </form>
     )
 }
